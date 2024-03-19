4fe8c54a02526abf816ed0f76350983f6e7bade9
yraffic02 <yraffic02@gmail.com>
2024-03-18 22:41:12 -0300

    feat: criado config sequelize

---
+++ src/db/database.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+    process.env.MYSQL_DATABASE,
+    process.env.MYSQL_USER, 
+    process.env.MYSQL_ROOT_PASSWORD, 
+    {
+        host: process.env.MYSQL_HOST, 
+        port: process.env.MYSQL_PORT, 
+        dialect: "mysql"
+    }
+);
+
+module.exports = sequelize;
