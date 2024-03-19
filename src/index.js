3fa6235c624759dff153e15e224adcadd46fca35
yraffic02 <yraffic02@gmail.com>
2024-03-18 22:42:27 -0300

    feat: configurações iniciais feitas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+require("dotenv").config()
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes');
+const connectDB = require('./db/connection');
 const app = express();
 
 const port = process.env.PORT_API || 3333
+
+//iniciando conexão com db
+connectDB()
+
 app.use(express.json())
+app.use(cors())
 app.use(router)
 
 app.listen(port, ()=>{
