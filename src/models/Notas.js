0010252f9d81bf9f93ff3c8f9b13daf2ece63884
yraffic02 <yraffic02@gmail.com>
2024-03-19 23:21:07 -0300

    feat: criada nova model

---
+++ src/models/Notas.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Notas extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Notas.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    titulo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    conteudo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }, {
+    sequelize,
+    modelName: 'Notas',
+    timestamps: false 
+  });
+  return Notas;
+};
