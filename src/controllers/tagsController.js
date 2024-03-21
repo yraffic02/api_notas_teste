93e93c8bdec3168cb344a3200515558e30ef76ea
yraffic02 <yraffic02@gmail.com>
2024-03-20 22:50:05 -0300

    chore: assoiação de notas para tags

--- src/controllers/tagsController.js
+++ src/controllers/tagsController.js
@@ -1,12 +1,16 @@
-const { Tags } = require("../models");
+const { Tags, Notas } = require("../models");
 
 async function registerTag(req, res){
   try {
-    const { titulo } = req.body;
+    const { titulo, notas } = req.body;
 
     const novaTag = await Tags.create({
       titulo
     });  
+
+    if(notas && notas.length > 0){
+      novaTag.setNotas(notas)
+    }
 
     return res.status(201).json(novaTag);
   } catch (err) {
@@ -16,10 +20,17 @@ async function registerTag(req, res){
 
 async function getAllTags(req, res){
   try {
-    const tags = await Tags.findAll();  
+    const tags = await Tags.findAll({ include: [
+      { 
+        model: Notas, 
+        as: 'Notas',
+        through: { attributes: []}
+      }
+    ] });  
 
     return res.status(200).json(tags);
   } catch (err) {
+    console.log(err)
     return res.status(500).json(err);
   }
 }
@@ -28,7 +39,13 @@ async function getTag(req, res){
   try {
     const { id } = req.params;
 
-    const tag = await Tags.findByPk(id);  
+    const tag = await Tags.findByPk(id, { include: [
+      { 
+        model: Notas, 
+        as: 'Notas',
+        through: { attributes: []}
+      }
+    ] });  
 
     if(!tag){
       return res.status(404).json({mensage: "tag Not found!"});
@@ -43,7 +60,7 @@ async function getTag(req, res){
 async function updateTag(req, res){
   try {
     const { id } = req.params;
-    const { titulo } = req.body;
+    const { titulo, notas } = req.body;
 
     const tag = await Tags.findByPk(id);  
 
@@ -57,9 +74,19 @@ async function updateTag(req, res){
       where: {
         id: id
       }
-    });  
+    });
+    
+    if(notas && notas.length > 0){
+      tag.setNotas(notas)
+    }
 
-    const updatedTag = await Tags.findByPk(id);  
+    const updatedTag = await Tags.findByPk(id, { include: [
+      { 
+        model: Notas, 
+        as: 'Notas',
+        through: { attributes: []}
+      }
+    ] });  
     
     return res.status(202).json(updatedTag);
   } catch (err) {
